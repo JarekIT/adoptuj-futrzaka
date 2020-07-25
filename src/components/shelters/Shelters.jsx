33b5f34dded2b6cfad9f9333ed70bbc67313e2bc
JarekIT <JaroslawDanielewiczIT@gmail.com>
2020-07-25 12:28:53 +0200

    add Shelters component to view all shelters

---
+++ src/components/shelters/Shelters.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+
+function Shelters({ shelters }) {
+  useEffect(() => {}, [shelters]);
+
+  return (
+    <div>
+      <h4>Lista schronisk</h4>
+      {shelters.map((shelter) => {
+        return (
+          <div key={shelter.name}>
+            {shelter.name} (kontakt: <u>{shelter.tel}</u>)
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Shelters;
