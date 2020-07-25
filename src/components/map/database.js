793beb6c0462b3af00b3162edcf1028918cb5926
JarekIT <JaroslawDanielewiczIT@gmail.com>
2020-07-25 12:47:13 +0200

    add city to json database

--- src/components/map/database.js
+++ src/components/map/database.js
@@ -6,18 +6,21 @@ const jsonData = [
     name: "OTOZ Animals Schronisko dla Bezdomnych Zwierząt Ciapkowo",
     lat: 54.4927268,
     lng: 18.5276672,
+    city: "Gdynia",
     tel: "58 622 25 52",
   },
   {
     name: "Towarzystwo Opieki nad Zwierzętami w Polsce, Oddział w Sopocie",
     lat: 54.4531541,
     lng: 18.5398991,
+    city: "Sopot",
     tel: "58 624 00 00",
   },
   {
     name: "Schronisko dla Bezdomnych Zwierząt PROMYK",
     lat: 54.3747584,
     lng: 18.4493279,
+    city: "Gdańsk",
     tel: "58 624 00 00",
   },
   {
@@ -25,6 +28,7 @@ const jsonData = [
       "OTOZ ANIMALS Schronisko dla Bezdomnych Zwierząt w Dąbrówce k. Wejherowa",
     lat: 54.563265,
     lng: 18.176634,
+    city: "Wejherowo",
     tel: "607 540 557",
   },
 ];
