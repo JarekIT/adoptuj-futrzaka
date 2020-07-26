fc4ab44f1844c38d2c828808a8f4af03f05d3e5c
JarekIT <JaroslawDanielewiczIT@gmail.com>
2020-07-26 18:18:26 +0200

    add Animal component to view picture of your favorite animals

---
+++ src/components/likesystem/Animal.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Actions from "./Actions";
+
+const Animal = ({ animal, modifySuperficialChoices }) => {
+  const { name, desc, age, image } = animal;
+
+  return (
+    <div className="animal">
+      <div className="animal-photo">
+        <img src={`/images/animals/${image}`} alt={name} />
+      </div>
+
+      <div className="animal-description">
+        <p className="animal-name-age">
+          {name}, <span>{age}</span>
+        </p>
+        <p className="animal-info">{desc}</p>
+      </div>
+
+      <Actions
+        animal={animal}
+        modifySuperficialChoices={modifySuperficialChoices}
+      />
+    </div>
+  );
+};
+
+export default Animal;
