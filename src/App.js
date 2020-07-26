4e6aedd3971b93ee574db2b81eb7c603438c0477
JarekIT <JaroslawDanielewiczIT@gmail.com>
2020-07-26 18:43:11 +0200

    Modify App.js to view all created components in single page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Map from "./components/map/Map";
+import AddShelter from "./components/addshelter/AddShelter.jsx";
+import Shelters from "./components/shelters/Shelters.jsx";
+import mockData from "./components/map/database";
+import LikeSystem from "./components/likesystem/LikeSystem.jsx";
+import databaseAnimals from "./components/likesystem/databaseAnimals.json";
+import databaseUsers from "./components/likesystem/databaseUsers.json";
+import ShowLikedAnimals from "./components/likesystem/ShowLikedAnimals.jsx";
 
 function App() {
+  const [shelters, setShelters] = useState([]);
+  const [users, setUsers] = useState(databaseUsers);
+  const [animals, setAnimals] = useState(databaseAnimals);
+  const [user, setUser] = useState(databaseUsers[0]);
+
+  useEffect(() => {
+    setShelters(mockData);
+    setAnimals(databaseAnimals);
+    setUsers(databaseUsers);
+    setUser(databaseUsers[0]);
+  }, []);
+
+  useEffect(() => {}, [shelters]);
+
   return (
-    <div>
-      <Map /> 
-    </div>
-  )}
+    <React.Fragment>
+      <div id="created-by-react">
+        <Map shelters={shelters} />
+        <hr />
+        <LikeSystem user={user} animals={animals} setAnimals={setAnimals} />
+        <hr />
+        <ShowLikedAnimals user={user} animals={animals} />
+        <hr />
+        <AddShelter shelters={shelters} setShelters={setShelters} />
+        <hr />
+        <Shelters shelters={shelters} />
+        <hr />
+      </div>
+    </React.Fragment>
+  );
+}
 
 export default App;
