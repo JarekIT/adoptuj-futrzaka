460773c549985ae80e5b9e2983f31ccb3bf2808a
JarekIT <JaroslawDanielewiczIT@gmail.com>
2020-07-27 14:09:05 +0200

    add id to shelter db and rename file

--- src/components/map/databaseShelters.js
+++ src/components/map/databaseShelters.js
@@ -3,6 +3,7 @@
 
 const jsonData = [
   {
+    id: 1,
     name: "OTOZ Animals Schronisko dla Bezdomnych Zwierząt Ciapkowo",
     lat: 54.4927268,
     lng: 18.5276672,
@@ -10,6 +11,7 @@ const jsonData = [
     tel: "58 622 25 52",
   },
   {
+    id: 2,
     name: "Towarzystwo Opieki nad Zwierzętami w Polsce, Oddział w Sopocie",
     lat: 54.4531541,
     lng: 18.5398991,
@@ -17,6 +19,7 @@ const jsonData = [
     tel: "58 624 00 00",
   },
   {
+    id: 3,
     name: "Schronisko dla Bezdomnych Zwierząt PROMYK",
     lat: 54.3747584,
     lng: 18.4493279,
@@ -24,6 +27,7 @@ const jsonData = [
     tel: "58 624 00 00",
   },
   {
+    id: 4,
     name:
       "OTOZ ANIMALS Schronisko dla Bezdomnych Zwierząt w Dąbrówce k. Wejherowa",
     lat: 54.563265,
