28ead55670dd4c376cb0a2a2bdd78ce292768e32
JarekIT <JaroslawDanielewiczIT@gmail.com>
2020-10-04 12:13:16 +0200

    add SuspenseErrorBoundary component for handling errors

---
+++ src/components/SuspenseErrorBoundary/SuspenseErrorBoundary.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { LoadingIndicator } from "components";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    return (
+      <React.Suspense fallback={LoadingIndicator}>
+        {this.state.hasError ? (
+          <div>
+            Coś poszło nie tak!
+          </div>
+        ) : (
+          <React.Fragment>{this.props.children}</React.Fragment>
+        )}
+      </React.Suspense>
+    );
+  }
+}
+
+export default ErrorBoundary;
