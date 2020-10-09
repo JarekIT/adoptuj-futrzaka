001a8df8eb473a99e749ed3559b56e2a3e11ca59
JarekIT <JaroslawDanielewiczIT@gmail.com>
2020-10-09 15:09:10 +0200

    add global Store for ContextAPI to use useReducer hook

---
+++ src/data/store/Store.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import { IState, IAction } from "../../interfaces/Store";
+
+import databaseAnimals from "../mocks/databaseAnimals.json";
+import mockData from "../mocks/databaseShelters";
+import databaseUsers from "../mocks/databaseUsers.json";
+
+export const initialState: IState = {
+  allAnimals: databaseAnimals,
+  animals: databaseAnimals,
+  shelters: mockData,
+  user: databaseUsers[0],
+};
+
+export const Store = React.createContext<IState | any>(initialState);
+
+console.log(Store);
+
+function reducer(state: IState, action: IAction): IState {
+  switch (action.type) {
+    case "FETCH_DATA_ANIMALS":
+      return {
+        ...state,
+        allAnimals: action.payload,
+        animals: action.payload,
+      };
+    case "FETCH_DATA_SHELTERS":
+      return {
+        ...state,
+        shelters: action.payload,
+      };
+    case "FETCH_DATA_USER":
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case "MODIFY_USER":
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case "MODIFY_ANIMALS":
+      return {
+        ...state,
+        animals: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export function StoreProvider({
+  children,
+}: JSX.ElementChildrenAttribute): JSX.Element {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  return (
+    <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
+  );
+}
