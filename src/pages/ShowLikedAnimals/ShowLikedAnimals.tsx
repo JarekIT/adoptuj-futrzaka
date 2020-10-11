7285ac2b7f10cab44f66e505901a9db062d73387
JarekIT <JaroslawDanielewiczIT@gmail.com>
2020-10-11 13:10:31 +0200

    improve UX/UI group liked animals by shelter localisation in ShowLikedAnimals page

--- src/pages/ShowLikedAnimals/ShowLikedAnimals.tsx
+++ src/pages/ShowLikedAnimals/ShowLikedAnimals.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { groupBy } from "lodash";
 
 import LikedAnimal from "./LikedAnimal/LikedAnimal";
 import LikedAnimalsShelterMap from "./LikedAnimalsSheltersMap/LikedAnimalsSheltersMap";
@@ -6,10 +7,21 @@ import LikedAnimalsShelterMap from "./LikedAnimalsSheltersMap/LikedAnimalsShelte
 import { Store } from "../../data/store/Store";
 
 import { AnimalDAO } from "../../interfaces/Animal";
+import { ShelterDAO } from "../../interfaces/Shelter";
 
 const LikedAnimals: React.FC = () => {
   const { state } = useContext(Store);
-  const { animals, user } = state;
+  const { animals, user, shelters } = state;
+
+  type MapGroupedAnimals = Record<number, AnimalDAO[]>;
+  type MapGroupedShelters = Record<number, ShelterDAO[]>;
+
+  const groupedAnimalsByShelterId: MapGroupedAnimals = groupBy(
+    user.likedAnimals,
+    "shelterId"
+  );
+
+  const groupedSheltersById: MapGroupedShelters = groupBy(shelters, "id");
 
   return (
     <div className="app">
@@ -23,12 +35,23 @@ const LikedAnimals: React.FC = () => {
             <p>
               {user.likedAnimals.length > 0
                 ? "Zwierzaki, które polubiles:"
-                : ""}
+                : "Nie ma żadnych polubionych zwierzaków"}
             </p>
 
-            {user.likedAnimals.map((animal: AnimalDAO) => (
-              <LikedAnimal key={animal.id} animal={animal} />
-            ))}
+            {Object.entries(groupedAnimalsByShelterId).map(
+              ([shelterId, groupedAnimals]) => (
+                <div>
+                  <span>{groupedSheltersById[shelterId][0].name}</span>
+                  <br />
+                  <span>{groupedSheltersById[shelterId][0].city}</span>
+                  <br />
+                  {groupedAnimals.map((animal: AnimalDAO) => (
+                    <LikedAnimal key={animal.id} animal={animal} />
+                  ))}
+                  <hr />
+                </div>
+              )
+            )}
           </div>
         </div>
       </div>
